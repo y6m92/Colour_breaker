0c7590f1c7ece77e35fc8de51df182d4c26794ab
y6m92 <y6m92@students.keele.ac.uk>
2026-03-14 23:18:49 +0000

    Add colour list and random code

--- game.py
+++ game.py
@@ -1,1 +1,13 @@
+import random
+
 print(" Welcome to Colour Code Breaker! ")
+print("Try to guess the colours.\n")
+
+colours = ["red", "blue", "green", "yellow", "purple"]
+print("Available colours:", ", ".join(colours))
+
+secret = [random.choice(colours) for _ in range(3)]
+
+
+
+
