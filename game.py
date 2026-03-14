a7131a7d33cd51ff206f4e23faae624fb0acf024
y6m92 <y6m92@students.keele.ac.uk>
2026-03-14 23:55:18 +0000

    Add guessing loop and handle basic input

--- game.py
+++ game.py
@@ -8,6 +8,25 @@ print("Available colours:", ", ".join(colours))
 
 secret = [random.choice(colours) for _ in range(3)]
 
+attempts = 5
+won = False
+
+while attempts > 0 and not won:
+    print("\nAttempts left:", attempts)
+    guess_input = input("Enter three colours separated by spaces: ").lower()
+    guess_list = guess_input.split()
+    
+    if len(guess_list) != 3:
+        print("Please enter exactly three colours.")
+        continue
+    
+    if guess_list == secret:
+        print("You cracked the code!")
+        won = True
+        break
+    else:
+        print("Not quite, hints coming soon...")
+        attempts -= 1
 
 
 
