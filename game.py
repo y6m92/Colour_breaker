b888b018f8da603b6d8c0ed9b0c4f13ab1fa1eac
y6m92 <y6m92@students.keele.ac.uk>
2026-03-15 19:59:30 +0000

    Start GUI file with basic labels

--- game.py
+++ game.py
@@ -1,16 +1,23 @@
+
+
 import random
 
+# Welcome message 
 print(" Welcome to Colour Code Breaker! ")
 print("Try to guess the colours.\n")
 
+# List of colours
 colours = ["red", "blue", "green", "yellow", "purple"]
 print("Available colours:", ", ".join(colours))
 
+# Randomly generate the secret code (3 colours)
 secret = [random.choice(colours) for _ in range(3)]
 
+# user can input 5 attempts
 attempts = 5
 won = False
 
+# Main game loop
 while attempts > 0 and not won:
     print("\nAttempts left:", attempts)
     guess_input = input("Enter three colours separated by spaces: ").lower()
@@ -37,16 +44,20 @@ while attempts > 0 and not won:
         else:
             hints.append("Not used")
             
+    # Display hints            
     print("Hints:")
+    
     for i in range(3):
         print(f"{guess_list[i]} -> {hints[i]}")
         
     attempts -= 1
 
+# If user loses
 if not won:
     print("\nYou ran out of attempts.")
     print("The secret code was:"," ".join(secret))
     
+# Ask if user wants to play again    
 play_again = input("\nPlay again? (y/n): ").lower()
 if play_again == "y":
     print("Restart the game to play again.")
---
+++ gui_game.py
@@ -0,0 +1,9 @@
+import tkinter as tk
+
+win = tk.Tk()
+win.title(" Colour Code Breaker ")
+
+label = tk.Label(win, text="Welcome to Colour Code Breaker!")
+label.pack(pady=20)
+
+win.mainloop()
