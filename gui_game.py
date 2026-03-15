3707e9525dc3753617be5335f697589beebf83ee
y6m92 <y6m92@students.keele.ac.uk>
2026-03-15 20:10:45 +0000

    Add instruction label and entry box

--- gui_game.py
+++ gui_game.py
@@ -1,9 +1,20 @@
 import tkinter as tk
+import random
 
+# Create main window
 win = tk.Tk()
 win.title(" Colour Code Breaker ")
 
+# List of colours
+colours = ["red", "blue", "green", "yellow", "purple"]
+
 label = tk.Label(win, text="Welcome to Colour Code Breaker!")
 label.pack(pady=20)
+
+instruction = tk.Label(win, text="Enter three colours separated by spaces: ")
+instruction.pack()
+
+entry = tk.Entry(win, width=40)
+entry.pack(pady=5)
 
 win.mainloop()
